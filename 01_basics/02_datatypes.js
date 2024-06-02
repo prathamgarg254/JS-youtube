0c80f5e2a6c5ebf62603a61c6b433daa3c5952c1
prathamgarg254 <156519142+prathamgarg254@users.noreply.github.com>
2024-06-02 10:26:27 +0000

    datypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,12 @@
+//  number -> 2 to power 53
+// string
+// bolean -> true or false
+//null 
+//undefined
+//symbol -> unique
+
+//object
+
+console.log(typeof "Pratham")
+console.log(typeof undefined)
+console.log(typeof NULL)
