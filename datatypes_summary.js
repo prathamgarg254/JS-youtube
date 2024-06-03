4e263a153d9948cd8106c57fc67d5e4eadffdd19
prathamgarg254 <156519142+prathamgarg254@users.noreply.github.com>
2024-06-03 09:30:24 +0000

    datatypes summary

---
+++ datatypes_summary.js
@@ -0,0 +1,18 @@
+//Primitive
+// 7 types = String,Number,Boolean,null,undefined,Symbol,BigInt
+
+//Refernce(Non primitive)
+//Arrays, object, function
+
+const heroes = ["iron man" , "hulk" , "thor"];
+let myobj = {
+    name: "abc",
+    age: "18",
+}
+
+const myfunction = function(){
+    console.log("Hello");
+}
+
+// ****null's type of object
+// Every non primitive data type is object  and functions is function (can be said as object function)
