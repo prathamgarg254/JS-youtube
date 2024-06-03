63657af9e7339140f7e65e9735578fd2f0d29777
prathamgarg254 <156519142+prathamgarg254@users.noreply.github.com>
2024-06-03 06:05:12 +0000

    Conversion

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,44 @@
+let score = 33
+
+let ValueInNumber = Number(score)
+
+//console.log(typeof ValueInNumber)
+//console.log(ValueInNumber)
+
+score = "33"
+
+ValueInNumber = Number(score)
+
+//console.log(typeof ValueInNumber)
+//console.log(ValueInNumber)
+
+score = "33abc"
+
+ValueInNumber = Number(score)
+
+//console.log(typeof ValueInNumber)
+//console.log(ValueInNumber)
+
+score = "Pratham"
+
+ValueInNumber = Number(score)
+
+//console.log(typeof ValueInNumber)
+//console.log(ValueInNumber)
+
+score = false
+
+ValueInNumber = Number(score)
+
+//console.log(typeof ValueInNumber)
+//console.log(ValueInNumber)
+
+let LoggedIn = ""
+
+let IsLoggedIn = Boolean(LoggedIn)
+
+console.log(IsLoggedIn)
+
+// 1 -> true ; 0 -> false
+// "" -> false
+// "abc" -> true
