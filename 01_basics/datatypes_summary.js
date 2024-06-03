21e8485c2a47f0fd334b057ccb89a609f441532b
prathamgarg254 <156519142+prathamgarg254@users.noreply.github.com>
2024-06-03 09:42:10 +0000

    Stack and Heap memory

--- 01_basics/datatypes_summary.js
+++ 01_basics/datatypes_summary.js
@@ -16,3 +16,29 @@ const myfunction = function(){
 
 // ****null's type of object
 // Every non primitive data type is object  and functions is function (can be said as object function)
+
+// *********************************************************************************************
+
+// Types of memory in JavaScript
+//Stack(primitive) , Heap(Non Primitive)
+
+// Stack creates a copy whereas Heap uses pass by refernce
+
+let user1 = "Pratham"
+let user2 = user1;
+user2 = "Anshuman"
+
+console.log(user2);
+console.log(user1);
+
+let user3 = {
+    email: "abc@gmail.com",
+    id: 123,
+}
+
+let user4 = user3;
+
+user4.email = "def@gmail.com"
+
+console.log(user4.email);
+console.log(user3.email);
