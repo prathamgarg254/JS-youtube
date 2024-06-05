81ce17c03edbe42a33b3ae10abc5ca92d8632f4e
prathamgarg254 <156519142+prathamgarg254@users.noreply.github.com>
2024-06-05 07:04:21 +0000

    numbers and math

---
+++ 01_basics/06_numbers.js
@@ -0,0 +1,47 @@
+/*const balance = 100
+
+const newbalance = new Number(200)
+
+console.log(balance);
+console.log(newbalance);
+
+console.log(newbalance.toString());
+console.log(newbalance.toString().length);
+
+const otherNumber = 24.898
+console.log(otherNumber.toPrecision(3))
+
+const newNumber = 124.898
+console.log(newNumber.toPrecision(3))
+
+const newestNumber = 1124.898
+console.log(newestNumber.toPrecision(3))
+
+const hundred = 1000000
+console.log(hundred.toLocaleString());
+console.log(hundred.toLocaleString('en-IN'));
+
+*/
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++ Maths +++++++++++++++++++++++++++++++++++++++++++++
+
+/*console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.3));
+console.log(Math.ceil(4.3));
+console.log(Math.floor(4.9));
+console.log(Math.min( 3 , 5 , 2 , 9));
+*/
+
+//console.log(Math.random());  //Always in between 0 and 1
+
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+// 1 is added to avoid zeroes
+
