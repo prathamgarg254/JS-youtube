8676faf212452209480e92f02dee3436cc2df941
prathamgarg254 <156519142+prathamgarg254@users.noreply.github.com>
2024-06-10 10:08:38 +0000

    functions2

--- 03_basics./01_functions.js
+++ 03_basics./01_functions.js
@@ -38,4 +38,44 @@ function loggedInMessage(username)
 
 //console.log(loggedInMessage("abc"));
 
-console.log(loggedInMessage());
+//console.log(loggedInMessage());
+
+// function calculateCartPrice (...num1)
+// {
+//     return num1
+// }
+
+// console.log(calculateCartPrice(40,50,60));
+
+function calculateCartPrice (val1 , val2 , ...num1)
+{
+    return num1
+}
+
+// console.log(calculateCartPrice(40,50,60,2000));
+
+const user = {
+    username: "abc",
+    price: 199
+}
+
+function handleObject(anyobject)
+{
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// handleObject(user)
+
+handleObject({
+    username: "abc",                 //Both ways can  work
+    price:99
+})
+
+const myNewArray = [200,400,600,800]
+
+function returnSecondValue(getArray)
+{
+    return getArray[1];
+}
+
+console.log(returnSecondValue(myNewArray));
