a29f2bd78f256c213fe63c495fc944701094faae
prathamgarg254 <156519142+prathamgarg254@users.noreply.github.com>
2024-06-11 04:25:33 +0000

    scope1

---
+++ 03_basics./02_scope.js
@@ -0,0 +1,10 @@
+var c = 300;
+
+if(true)
+    {
+        const a = 10
+        let b = 20
+        var c  = 30 //or c = 30
+    }
+
+console.log(c);  //The Problem in c is var has no scope , so use let instead
