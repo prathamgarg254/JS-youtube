26d58af6be3d78cc9a3598342f1cfb0e87ab7dd3
prathamgarg254 <156519142+prathamgarg254@users.noreply.github.com>
2024-06-11 05:25:11 +0000

    scope2

--- 03_basics./02_scope.js
+++ 03_basics./02_scope.js
@@ -7,4 +7,32 @@ if(true)
         var c  = 30 //or c = 30
     }
 
-console.log(c);  //The Problem in c is var has no scope , so use let instead
+// console.log(c);  //The Problem in c is var has no scope , so use let instead
+
+function one()
+{
+    const username = "hitesh"
+function two()
+{
+    const website = "youtube"
+    console.log(username);
+}
+console.log(website); //cannot access
+two()
+}
+one()
+
+
+addOne(5) //This will work properly
+function addOne(num)
+{
+    return num + 1
+}
+
+
+addTwo(5) //This will display error
+const addTwo = function(num)
+{
+    return num+2
+}
+
