c2295515f2f911a69907878d0076bbe16ecaf18d
prathamgarg254 <156519142+prathamgarg254@users.noreply.github.com>
2024-06-13 09:25:58 +0000

    for loop

---
+++ 05_iterations/one.js
@@ -0,0 +1,18 @@
+// for loop
+
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+    
+// }
+
+// for (let i = 0; i < 10; i++) {
+//     const element = i;
+//     console.log(element);
+// }
+
+let myArray = ['Flash' , 'Batman' , 'Superman']
+
+for (let i = 0; i < myArray.length; i++) {
+    const element = myArray[i];
+    console.log(element);
+}
