2845a69d3160cf161ade74838038f0b7612a3a1d
Pratham Garg <gargpratham254@gmail.com>
2024-06-29 14:02:14 +0530

    more in js

---
+++ 10_classes_oops/staticprop.js
@@ -0,0 +1,30 @@
+class User{
+    constructor(username){
+        this.username = username
+    }
+
+    logMe(){
+        console.log(`Username: ${this.username}`);
+    }
+
+    static createId(){
+        return `123`
+    }
+
+    //static is to from accesing
+}
+
+const hitesh = new User("hitesh")
+// console.log(hitesh.createId());
+
+class Teacher extends User{
+    constructor(username, email)
+    {
+        super(username)
+        this.email = email
+    }
+}
+
+const iphone = new Teacher("iphone", "i@phone.com")
+
+iphone.logMe()
